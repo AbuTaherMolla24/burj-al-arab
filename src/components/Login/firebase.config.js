2c7709b13c679aadb7478aa7e8acd0b8d082489b
Abu Taher Molla <57549941+AbuTaherMolla24@users.noreply.github.com>
2020-09-27 19:21:07 +0600

    end

---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCY3qT4IwUn2KBowe2sA8Y4i8ndV1d7y3Q",
+    authDomain: "burj-al-arab-f8824.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-f8824.firebaseio.com",
+    projectId: "burj-al-arab-f8824",
+    storageBucket: "burj-al-arab-f8824.appspot.com",
+    messagingSenderId: "785832059342",
+    appId: "1:785832059342:web:09dd5c3a0364f58eb8cb9f"
+  };
+  export default firebaseConfig;
